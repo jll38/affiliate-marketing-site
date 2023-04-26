d60a527eb1724c9f0e19d39550118c17bc5f605a
Julian Lechner <jll38@njit.edu>
2023-04-25 23:38:59 -0400

    card sizing

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -5,23 +5,24 @@ import ScaleOnHover from "./ScaleOnHover";
 function BlogCard({ ...props }) {
   return (
     <ScaleOnHover>
-      <div className="max-w-sm max-h-96 rounded overflow-hidden my-3">
+      <div className="max-w-sm h-80 rounded overflow-hidden my-3">
         <img
-          className="w-full h-52 px-6"
+          className="w-full px-6 max-h-52"
           src={props.img}
           alt="Sunset in the mountains"
         />
-        
+
         <div className="px-6">
-       
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2">
-            
             {props.category}
           </span>
         </div>
 
-        <div className="px-6 bottom:0">
+        <div className="px-6">
           <div className="font-bold text-xl">{props.children}</div>
+        </div>
+
+        <div className="absolute bottom-0 px-6 mt-auto">
           <Date dateString={props.date}></Date>
         </div>
       </div>
