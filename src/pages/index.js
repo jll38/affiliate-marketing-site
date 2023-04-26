a2c78b360a271c2179df287d82351b534b294fd1
Julian Lechner <jll38@njit.edu>
2023-04-26 00:07:00 -0400

    Hero landing page mobile styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
           className="bg-secondary bg-[url('https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover"
         >
           <div className="pt-24 md:mt-0 md:h-screen flex justify-center text-center md:text-left md:flex-row md:justify-between items-center lg:px-48 md:px-12 px-4 backdrop-blur-sm bg-opacity-50 bg-gray-500">
-            <div className="md:flex-1 md:mr-10 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] text-center sm:my-20">
+            <div className="md:flex-1 md:mr-10 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] text-center my-32">
               <h1 className="font-pt-serif text-5xl font-bold mb-7 drop-shadow-md mb-0">
                 Empower Your Home
                 <br />
