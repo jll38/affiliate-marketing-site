54996fe4c50197242f71c692f9ecbe426b1e3689
Julian Lechner <jll38@njit.edu>
2023-05-29 19:38:12 -0400

    products page subnav

--- src/pages/products.js
+++ src/pages/products.js
@@ -43,10 +43,34 @@ export default function Products() {
         name="Homesync Products"
         className="pt-32 md:m-0 md:min-h-screen md:text-left lg:px-48 md:px-12 px-4 text-black bg-gray-100 animate-fadein"
       >
-        <h1 className="text-3xl font-semibold text-center md:text-left">
-          Featured Products
+        <h1 className="text-6xl font-semibold text-center mb-5 text-gray-800">
+          Home Sync Hub
         </h1>
         <Divider />
+        <div className="flex justify-between mx-5">
+          <ul
+            name="categories"
+            className="flex gap-5 text-gray-600 child-hover:text-gray-900"
+          >
+            <li>Entertainment</li>
+            <li>Comfort</li>
+            <li>Security</li>
+            <li>Health & Wellness</li>
+          </ul>
+          <ul name="socials" className="flex gap-2 text-red-600">
+            <li>
+              <i className="fa-brands fa-instagram"></i>
+            </li>
+            <li>
+              <i className="fa-brands fa-facebook"></i>
+            </li>
+            <li>
+              <i className="fa-brands fa-twitter"></i>
+            </li>
+          </ul>
+        </div>
+        <Divider />
+        <h1 className="text-2xl font-medium mt-5 text-gray-800">Featured Products</h1>
         <div className="flex flex-row flex-wrap lg:justify-start md:items-center justify-center">
           {loading ? (
             <>{placeHolders.map((placeholder) => placeholder)}</>
